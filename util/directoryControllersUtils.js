e700599a16cedebed18124008ad5d511a996836f
Robin Singh <robinsingh1117@gmail.com>
2026-08-29 20:27:37 +0530

    fixing recursive delete functionality

--- util/directoryControllersUtils.js
+++ util/directoryControllersUtils.js
@@ -3,6 +3,8 @@ import directoryModel from "../models/directoryModel.js";
 import fileModel from "../models/fileModel.js";
 import { ObjectId } from "mongodb";
 import path from "node:path";
+import { AbortMultipartUploadCommand, DeleteObjectCommand } from "@aws-sdk/client-s3";
+import s3Client from "../config/s3Config.js";
 
 //This function recursively delete file and filers from directory
 export async function recursiveDeletionDirectory(id, req) {
@@ -19,19 +21,30 @@ export async function recursiveDeletionDirectory(id, req) {
   if (fileCollection.length) {
     for await (let fileObject of fileCollection) {
       let fileid = fileObject._id;
-      let fullName = `${fileid}${fileObject.extension}`;
       try {
-        await rm(path.join(import.meta.dirname, "/../storage", fullName));
+        if (fileObject.uploadStatus !== "completed") {
+          const abortingIncompleteFile = new AbortMultipartUploadCommand({
+            Bucket: process.env.AWS_BUCKET_NAME,
+            Key: fileid.toString(),
+            UploadId: fileObject.uploadId,
+          });
+          await s3Client.send(abortingIncompleteFile);
+        } else {
+          const deleteCommand = new DeleteObjectCommand({
+            Bucket: process.env.AWS_BUCKET_NAME,
+            Key: fileid.toString(),
+          });
+          await s3Client.send(deleteCommand);
+        }
         await fileModel.deleteOne({ _id: new ObjectId(fileid) });
       } catch (error) {
         console.log("file Not found or deleted already", error);
       }
     }
   }
-  
+
   if (directCollection.length) {
     for (let dirObject of directCollection) {
-      console.log(dirObject._id);
       await recursiveDeletionDirectory(dirObject._id, req);
     }
   }
